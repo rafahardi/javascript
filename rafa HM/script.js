4acef125bf689d2628f26f6ee48c4da23272170d
rafahardi <rafa.hardian@student.alhazen.academy>
2024-10-18 14:07:54 +0700

    Add files via upload

---
+++ rafa HM/script.js
@@ -0,0 +1,25 @@
+var nama="ahmad";
+console.log(nama);
+nama = "budi"
+console.log(nama);
+var nama = "asep"
+console.log(nama);
+let umur="20";
+console.log(umur);
+ umur = "31"
+console.log(umur);
+const alamat ="bogor"
+console.log(alamat);
+angka1 = 13;
+angka2 = 10 ;
+console.log(6+2);
+console.log(6-2);
+hasil = angka1*angka2;
+console.log(hasil);
+let number1 =prompt("masukkan angka pertama : ");
+let number2 =prompt("masukkan angka kedua : ");
+
+let result= parseInt(number1) + parseInt(number2);
+alert(result);
+fruit=["banana","apple","orange","durian"];
+alert(fruit[2]);
